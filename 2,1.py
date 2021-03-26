8423db97b5a4930f9963117f930eae33fd05372f
Vlad-Russ <70793269+Vlad-Russ@users.noreply.github.com>
2021-03-26 12:04:18 +0300

    Add files via upload

--- 2,1.py
+++ 2,1.py
@@ -1,18 +1,30 @@
 from anytree import Node, RenderTree
 
 x0 = Node("x0")
-nix = Node("nix", parent=x0)
-eq = Node("eq", parent=nix)
-elm = Node("elm", parent=eq)
-a = Node("1981", parent=elm)
-a = Node("1957", parent=elm)
-pog = Node("pog", parent=eq)
-a = Node("1981", parent=pog)
-a = Node("2019", parent=pog)
-dm = Node("dm", parent=nix)
-lsl = Node("lsl", parent=nix)
-sage = Node("sage", parent=x0)
-ioke = Node("ioke", parent=x0)
+pawn = Node("pawn", parent=x0)
+a = Node("1984", parent=pawn)
+m = Node("m", parent=a)
+cobol = Node("cobol", parent=a)
+a1 = Node("2014", parent=cobol)
+a2 = Node("1982", parent=cobol)
+a3 = Node("1982", parent=cobol)
+
+b = Node("1986", parent=pawn)
+xojo = Node("xojo", parent=b)
+oz = Node("oz", parent=b)
+b1 = Node("2014", parent=oz)
+b2 = Node("1982", parent=oz)
+b3 = Node("2011", parent=oz)
+
+c = Node("1973", parent=pawn)
+m = Node("m", parent=c)
+xojo = Node("xojo", parent=m)
+oz = Node("oz", parent=m)
+
+cobol = Node("cobol", parent=c)
+
+blade = Node("blade", parent=x0)
+clips = Node("clips", parent=x0)
 
 print(x0)
 Node('/x0')
