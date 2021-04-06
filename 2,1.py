234f8ff929a36fa8ad3d35872834a6025c615a85
Vlad-Russ <70793269+Vlad-Russ@users.noreply.github.com>
2021-04-06 11:51:50 +0300

    1
    1

--- 2,1.py
+++ 2,1.py
@@ -1,32 +1,11 @@
 
-rez = -1
-f = [1973, 'http', 'dylan', 'forth', 1997]
-if f[2] == 'dylan':
-    rez = 9
-if f[2] == 'tcsh':
-    rez = 10
-if (f[3] == 'arc') & (rez == -1):
-    rez = 11
-if rez == -1:
-    if f[0] == 2001:
-        if f[4] == 2000:
-            rez = 0
-        if f[4] == 1997:
-            rez = 1
-        if f[4] == 1965:
-            rez = 2
-    if f[0] == 1973:
-        if f[4] == 2000:
-            rez = 3
-        if f[4] == 1997:
-            rez = 4
-        if f[4] == 1965:
-            rez = 5
-    if f[0] == 1995:
-        if f[4] == 2000:
-            rez = 6
-        if f[4] == 1997:
-            rez = 7
-        if f[4] == 1965:
-            rez = 8
-print(rez)
+fd = int(bin(2225759614), 2)
+fd1 = fd & 0b00001111000000000000000000000000
+fd2 = fd & 0b11110000000000000000000000000000
+fd3 = fd & 0b00000000111111111111111111111111
+fd2 = fd2 >> 4
+fd = fd2 | fd3
+fd1 = fd1 >> 24
+fd = fd << 4
+fd = fd | fd1
+print(fd)
