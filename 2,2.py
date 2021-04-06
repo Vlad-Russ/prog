b6819b76e5b53c7a8cbd50d4b0579b922e4b902a
Vlad-Russ <70793269+Vlad-Russ@users.noreply.github.com>
2021-04-06 10:02:45 +0300

    Update 2,2.py

--- 2,2.py
+++ 2,2.py
@@ -1,32 +1,37 @@
-
 rez = -1
-f = [1973, 'http', 'dylan', 'forth', 1997]
-if f[2] == 'dylan':
-    rez = 9
-if f[2] == 'tcsh':
-    rez = 10
-if (f[3] == 'arc') & (rez == -1):
+f = (['cobol', 1973, 'clips', 2011, 'oz'])
+if f[2] == 'blade' and (rez == -1):
     rez = 11
+if f[2] == 'clips' and (rez == -1):
+    rez = 12
+
 if rez == -1:
-    if f[0] == 2001:
-        if f[4] == 2000:
-            rez = 0
-        if f[4] == 1997:
+
+    if f[0] == 'cobol':
+        if f[4] == 2014:
             rez = 1
-        if f[4] == 1965:
+        if f[4] == 1982:
             rez = 2
-    if f[0] == 1973:
-        if f[4] == 2000:
+        if f[4] == 2011:
             rez = 3
-        if f[4] == 1997:
-            rez = 4
-        if f[4] == 1965:
-            rez = 5
-    if f[0] == 1995:
-        if f[4] == 2000:
-            rez = 6
-        if f[4] == 1997:
-            rez = 7
-        if f[4] == 1965:
+    elif f[3] == 'blade' or f[3] == 'clips':
+
+        rez = 10
+    if f[0] == 'm':
+
+        if f[4] == 'xojo':
             rez = 8
+        if f[4] == 'oz':
+            rez = 9
+    elif f[3] == 'blade' or f[3] == 'clips':
+
+        rez = 0
+
+if f[0] == 1995:
+    if f[4] == 2000:
+        rez = 6
+    if f[4] == 1997:
+        rez = 7
+    if f[4] == 1965:
+        rez = 8
 print(rez)
